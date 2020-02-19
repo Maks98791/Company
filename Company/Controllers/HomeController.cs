8218dcd233447746e986226264fc394cc6ed031d
Student 241353 <241353@student.pwr.edu.pl>
2020-02-19 16:20:22 +0100

    Add empty project prepared to work with

---
+++ Company/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Company.Models;
+
+namespace Company.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRespository)
+        {
+            _employeeRepository = employeeRespository;
+        }
+
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+
+        public ViewResult Details()
+        {
+            Employee employee = _employeeRepository.GetEmployee(1);
+            return View(employee);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Company/Models/MockEmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Michael", Surname = "Fold", Email = "michael341@gmail.com", Department = "IT" },
+                new Employee() { Id = 1, Name = "Lena", Surname = "Roy", Email = "lene33e@gmail.com", Department = "HR" },
+                new Employee() { Id = 1, Name = "Angie", Surname = "Herton", Email = "angie32@gmail.com", Department = "IT" }
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
